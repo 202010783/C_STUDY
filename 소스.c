2854473844b324d58493fa2de71aed614ee6382e
202010783 <79961143+202010783@users.noreply.github.com>
2021-07-16 16:52:29 +0900

    Add files via upload

---
+++ 소스.c
@@ -0,0 +1,164 @@
+//#include<stdio.h>
+//int main(void) {
+//	
+//	int j = 2;
+//	for (int i = 1; i <= 9; i++)
+//	{
+//		printf("%d * %d = %d\n", j, i, (i*j));
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main() {
+//	int i, j;
+//	int line;
+//
+//	printf("");
+//	scanf_s("%d", &line);
+//
+//	for (i = 0; i < line; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//#include<stdio.h>
+//int main(void)
+//{
+//	int A = 0;
+//	int B = 0;
+//
+//	while (1) {
+//		scanf_s("%d %d", &A, &B);
+//		if (A == 0 && B == 0) 
+//		{
+//			break;	
+//		}
+//		printf("%d\n", A + B);
+//	} 
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main(void) 
+//{
+//	int A = 0;
+//	int B = 0;
+//	int C = 0;
+//	int result = 0;
+//	int rest = 0;
+//	int Array[10] = { 0 };
+//
+//	scanf("%d", &A);
+//	scanf("%d", &B);
+//	scanf("%d", &C);
+//
+//	result = A * B * C;
+//
+//
+//}
+
+//#include<stdio.h>
+//int main() {
+//	int i, j, n, k;
+//
+//	scanf_s("%d", &n);
+//
+//	for (i = 1; i <= n; i++) 
+//	{
+//		
+//		for (j = 1; j <= n - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (k = 1; k <= i; k++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	
+//	
+//
+//}
+//#include<stdio.h>
+//int main(void) {
+//
+//	int S;
+//
+//	printf("");
+//	scanf_s("%d", &S);
+//
+//	if (90 <= S && S <= 100) {
+//		printf("A");
+//	}
+//	else if (80 <= S && S <= 89) {
+//		printf("B");
+//	}
+//	else if (70 <= S && S <= 79) {
+//		printf("C");
+//	}
+//
+//	else if (60 <= S && S <= 69) {
+//		printf("D");
+//	}
+//	else {
+//		printf("F");
+//	}
+//
+//}
+
+//int main(void) {
+//    int x, y;
+//
+//    scanf("%d%d", &x, &y);
+//
+//    if (x > 0 && y > 0) {
+//        printf("1");
+//    }
+//
+//    else if (x < 0 && y>0) {
+//        printf("2");
+//    }
+//
+//    else if (x > 0 && y < 0) {
+//        printf("3");
+//    }
+//    else {
+//        printf("4");
+//    }
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main(void) {
+//
+//	int i;
+//	int n;
+//	scanf_s("%d", &n);
+//
+//	for (int i = 1; i <= 9; i++)
+//	{
+//		printf("%d * %d = %d\n", n, i, (n * i));
+//	}
+//	return 0;
+//}
+
+#include<stdio.h>
+int main(void) {
+
+	int A = 0;
+	int B = 0;
+
+	while (scanf_s("%d %d", &A, &B) != EOF)
+	{
+		if((0<A&&A>10))
+	}
+
+}
